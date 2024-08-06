aac2154e1761c982ba9cbba7e70c99b9bf9322cb
Kim Khoi Lam <kimkhoi2202@gmail.com>
2024-08-06 18:15:12 -0500

    update Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,16 +7,19 @@ export default function HeroSection() {
     <HeroHighlight containerClassName="w-full bg-[url('/hero-bg.jpg')] bg-cover bg-center py-24 md:py-32 lg:py-40">
       <div className="container px-6 md:px-12 flex flex-col md:flex-row items-center justify-center text-center">
         <div className="w-full md:w-3/4 lg:w-2/3 mb-24 md:mb-0">
-          <span className="text-red-500 text-base">INTERNASH GLOBAL SERVICES</span>
-          <h1 className="mt-4 text-black dark:text-white text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
-            Engineering, Repair, and Logistics Solutions
-          </h1>
-          <p className="mt-3 text-black dark:text-white text-lg md:text-xl">
-            <Highlight>
-              Engineering, Repair, & Logistics solutions
-            </Highlight>create value and a competitive edge for our clients
-          </p>
-          <div className="mt-10 flex flex-col md:flex-row md:items-center md:justify-center">
+          <span className="text-red-500 text-base font-bold">INTERNASH GLOBAL SERVICES</span>
+            <h1 className="mt-4 text-black dark:text-white text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
+              <Highlight>Engineering, Repair,</Highlight>
+            </h1>
+            <h1 className="mt-4 text-black dark:text-white text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight">
+              <Highlight>and Logistics Solutions</Highlight>
+            </h1>
+          <div className="mt-4 flex flex-col items-center text-lg md:text-xl text-black dark:text-white">
+            <span className="mt-2">
+              Engineering, Repair, & Logistics solutions create value and a competitive edge for our clients
+            </span>
+          </div>
+          <div className="mt-5 flex flex-col md:flex-row md:items-center md:justify-center">
             <Button size="lg">Contact Us</Button>
           </div>
         </div>
